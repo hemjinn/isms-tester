e63d9f91dbeca7d031c26666d7733d98be1b7fcb
ssepssep <guswls7816@naver.com>
2023-12-25 22:34:04 +0900

    upload tester v0.9

---
+++ isms_auto.py
@@ -0,0 +1,35 @@
+from data import data, questions, choice
+import random
+
+print('''
+******************************************************
+isms 통제항목 자동 출제 프로그램 입니다.
+
+문제를 보고 항목 번호와 명칭을 입력하면 됩니다.
+ex) "항목 번호를 입력하세요: 1.1.1"
+    "항목 명을 입력하세요: 경영진의 참여"
+      
+종료는 exit 또는 ctrl+c를 눌러주세요.
+******************************************************     
+
+''')
+
+while True:
+    questionNum = random.choice(choice) # 랜덤으로 항목 번호 뽑기
+    question = random.choice(questions[questionNum]) # 랜덤으로 문제 뽑기
+    questionName = data[questionNum]
+
+    print("================================================================================================================")
+    print(question)
+    print("================================================================================================================")
+    guessNum = input("항목 번호를 입력하세요: ")
+    if (guessNum == "exit"):
+        break
+    guessName = input("항목 명을 입력하세요: ")
+
+    if (guessName == "exit"):
+        break
+    else:
+        if (guessNum == questionNum and guessName == questionName):
+            print("정답입니다.")
+        else: print("오답입니다.")
